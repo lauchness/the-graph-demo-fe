9e42d27fab2f766fd974cc8ed528f585b42098c9
lauchness <lauchlanchisholm@gmail.com>
2022-05-09 15:20:24 -0300

    Wrapping app in URQL provider

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,17 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import "../styles/globals.css";
+import type { AppProps } from "next/app";
+import { createClient, Provider } from "urql";
+
+const client = createClient({
+  url: String(process.env.NEXT_PUBLIC_GRAPH_API),
+});
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <Provider value={client}>
+      <Component {...pageProps} />
+    </Provider>
+  );
 }
 
-export default MyApp
+export default MyApp;
